b47261825e37fda637324ccc1ed1540429d7fb92
Masato Kiryu <kiri9120@gmail.com>
2020-05-02 22:02:26 +0900

    add page_top

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-console.log('hello');
+$(function () {
+  var appear = false;
+  var pagetop = $("#page_top");
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      //100pxスクロールしたら
+      pagetop.addClass("show");
+    } else {
+      pagetop.removeClass("show");
+    }
+  });
+  pagetop.click(function () {
+    $("body, html").animate({ scrollTop: 0 }, 500); //0.5秒かけてトップへ戻る
+    return false;
+  });
+});
